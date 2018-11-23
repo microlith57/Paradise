b49f4f17678d9fa869f8efea83b82a449745df80
microlith57 <microlith57@gmail.com>
2018-11-24 12:06:23 +1300

    Add exports to lisp_helpers.js Signed-off-by: microlith57 <microlith57@gmail.com>

--- desktop/server/core/lisp_helpers.js
+++ desktop/server/core/lisp_helpers.js
@@ -5,20 +5,22 @@ const errors  = require('./errors')
 const Error   = require('./error')
 const Vessel  = require('./vessel')
 
-function _prepare_lisp (a) {
-  if (a == null || isNaN(a) || a === false) {
-    return helpers.nil
-  } else if (typeof input === 'function') {
+const _prepare_lisp = function prepare_lisp (a, def = helpers.nil) {
+  if (a == null || (typeof a === 'number' && isNaN(a)) || a === false) {
+    return def
+  } else if (typeof a === 'function') {
     a = a()
   }
   if (a instanceof Error) {
+    return a
     // TODO: allow this case to break out of a function early, carrying the error further
+    //       Maybe even use `raise`
   }
   return a
 }
 
-function _vessel_from_id (context, id) {
-  id = _prepare_lisp(id)
+const _vessel_from_id = function vessel_from_id (context, id, def = helpers.nil) {
+  id = _prepare_lisp(id, def)
   if (typeof id === 'number' && Number.isInteger(id)) {
     const target = context.host.paradise.world[id]
     if (target) {
@@ -29,4 +31,9 @@ function _vessel_from_id (context, id) {
   } else {
     return helpers.nil // TODO: Raise error
   }
+}
+
+module.exports = {
+  prepare_lisp: _prepare_lisp,
+  vessel_from_id: _vessel_from_id,
 }
